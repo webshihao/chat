f5bb4c9cf70f1f195cd7b85673813a3b64e0af12
huke <huke@yiche.com>
2018-05-15 11:53:24 +0800

    fe

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var url  = require("url"),
     path = require("path");
 http.createServer(function (req, res) {
     var pathname=__dirname+url.parse(req.url).pathname;
-    console.log(pathname,123)
     if (path.extname(pathname)=="") {
         pathname+="/";
     }
@@ -31,30 +30,8 @@ http.createServer(function (req, res) {
     })
     fs.exists(pathname,function(exists){
         if(exists){
-            switch(path.extname(pathname)){
-                case ".html":
-                    res.writeHead(200, {"Content-Type": "text/html"});
-                    break;
-                case ".js":
-                    res.writeHead(200, {"Content-Type": "text/javascript"});
-                    break;
-                case ".css":
-                    res.writeHead(200, {"Content-Type": "text/css"});
-                    break;
-                case ".gif":
-                    res.writeHead(200, {"Content-Type": "image/gif"});
-                    break;
-                case ".jpg":
-                    res.writeHead(200, {"Content-Type": "image/jpeg"});
-                    break;
-                case ".png":
-                    res.writeHead(200, {"Content-Type": "image/png"});
-                    break;
-                default:
-                    res.writeHead(200, {"Content-Type": "application/octet-stream"});
-            }
-
             fs.readFile(pathname,function (err,data){
+                console.log(data)
                 res.end(data);
             });
         } else {
